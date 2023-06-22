4810ac706fdac8216186ef595d2c10a3afda2238
Jonas Rocha <jonasrochabr@gmail.com>
2023-06-22 10:39:50 -0400

    Commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
+require("express-async-errors")
+
+const AppError = require("./utils/appError")
+
 const express = require("express");
+
+const routes = require("./routes")
 
 const app = express();
 app.use(express.json())
 
+app.use(routes);
 
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body;
-    
-    response.send(`Usuário: ${name} - Email: ${email} - Password: ${password}`)
+app.use(( error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
 })
 
 const PORT = 3000;
