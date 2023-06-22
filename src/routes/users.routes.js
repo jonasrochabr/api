44fa9b9a65995d89c326b7d3caa3e617f70d2671
Jonas Rocha <jonasrochabr@gmail.com>
2023-06-22 15:05:27 -0400

    continue

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -12,7 +12,7 @@ const usersController = new UsersController();
 
 
 usersRoutes.post("/", usersController.create); 
-    
+usersRoutes.put("/:id", usersController.update);
 
 
 module.exports = usersRoutes;
